1665c5af8c9dc3a631159352ac35800efe5927fb
Cheng In Du <cdu@bayviewglen.ca>
2024-09-12 09:57:07 -0400

    remove subject

--- organizer.sh
+++ organizer.sh
@@ -63,6 +63,24 @@ archive_notes() {
         echo "No notes to archive for $subject."
     fi
 }
+# Function to remove subjects
+remove_subject () {
+    list_subjects
+    echo "Please select the subject/project that you want to delete: "
+    read subject
+    if [ -d "$HOME/personal-organizer/$subject" ]; then
+        echo "Are you sure you want to delete $subject and all of its contents? y or n"
+        read confirmation
+        if [ "$confirmation" == "y" ]; then
+            rm -r "$HOME/personal-organizer/$subject"
+            echo "Subject $subject has been deleted"
+        else
+            echo "Deletion cancelled"
+        fi
+    else
+        echo "Subject not found."
+    fi
+}
 # Main Menu
 while true; do
     echo "------------------------"
@@ -72,7 +90,8 @@ while true; do
     echo "3. View notes"
     echo "4. Search notes"
     echo "5. Archive notes"
-    echo "6. Exit"
+    echo "6. Delete a subject/project"
+    echo "7. Exit"
     echo "------------------------"
     echo "Choose an option:"
     read option
@@ -97,6 +116,10 @@ while true; do
             archive_notes
             ;;
         6)
+            list_subjects
+            remove_subject
+            ;;        
+        7)
             echo "Exiting..."
             exit 0
             ;;
